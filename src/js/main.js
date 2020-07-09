7d97fda59d4f7e64ddb3faa56f12b7703e465fc0
anapy <anabelenfunesc@gmail.com>
2020-07-09 21:37:41 +0200

    add error message and not found message

--- src/js/main.js
+++ src/js/main.js
@@ -10,7 +10,6 @@ function clickHandler(ev) {
   ev.preventDefault();
   cleanResults();
   serie = serieInput.value;
-  console.log(serie);
   getApiData();
 }
 
@@ -26,30 +25,50 @@ function getApiData() {
 //print the series given by the api
 //creates the elements by DOM appending to ul tag each li with its proper content
 function printResults() {
-  for(let i = 0; i < results.length; i++) {
-    const newLi = document.createElement('li');
-    newLi.classList.add('serie-container');
-    const liTitle = document.createElement('h2');
-    liTitle.classList.add('serie-title');
-    const liTitleContent = document.createTextNode(results[i].show.name);
-    newLi.appendChild(liTitle);
-    const liImg = document.createElement('IMG');
-    liImg.setAttribute('src', results[i].show.image.medium);
-    liImg.setAttribute('alt', results[i].show.name);
-    liImg.setAttribute('id', results[i].show.name);
-    newLi.appendChild(liImg);
-    resultList.appendChild(newLi);
+  if(results.length === 0) {
+    const error = document.createElement('p');
+    error.classList.add('error');
+    const errorContent = document.createTextNode('La serie buscada no está en nuestro listado, prueba de nuevo');
+    error.appendChild(errorContent);
+    resultList.appendChild(error);
+  } else {
+    for(let i = 0; i < results.length; i++) {
+      const newLi = document.createElement('li');
+      newLi.classList.add('serie-container');
+      const liTitle = document.createElement('h2');
+      liTitle.classList.add('serie-title');
+      const liTitleContent = document.createTextNode(results[i].show.name);
+      liTitle.appendChild(liTitleContent);
+      newLi.appendChild(liTitle);
+      const liImg = document.createElement('IMG');
+      checkImg(i, liImg);
+      liImg.setAttribute('alt', results[i].show.name);
+      liImg.setAttribute('id', results[i].show.name);
+      newLi.appendChild(liImg);
+      resultList.appendChild(newLi);
+    }
   }
+
 }
 
-//clean the last result removing all child from the ul tag
+//clean the last result removing all child from the ul tag or the error message
 function cleanResults () {
   const listItems = document.querySelectorAll('.serie-container');
   for(const child of listItems) {
     resultList.removeChild(child);
   }
-
+  const errors = document.querySelector('.error');
+  if(errors !== null) {
+    resultList.removeChild(errors);
+  }
 }
 
+//checkImg checks whether is an image on the result array//
+function checkImg(index, item) {
+  if(results[index].show.image.medium === null) {
+    item.setAttribute('src', 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV');
+  } else
+    item.setAttribute('src', results[index].show.image.medium);
+}
 
 searchButton.addEventListener('click', clickHandler);
