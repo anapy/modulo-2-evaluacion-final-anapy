756711dfbab9ff01cfebbe05b4b5451374bfd4e7
anapy <anabelenfunesc@gmail.com>
2020-07-10 19:41:39 +0200

    delete from favourites and fix details

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,17 @@
 
 const searchButton = document.querySelector('.js-search');
 const serieInput = document.querySelector('.js-serie');
-let resultList = document.querySelector('.js-list-results');
+const resultList = document.querySelector('.js-list-results');
+const favouriteList = document.querySelector('.js-favourite-list');
 let serie = '';
 let results = [];
+let resultItems = [];
 let favourites = [];
-let listItems = [];
+let favouriteItems = [];
 
 function clickHandler(ev) {
   ev.preventDefault();
-  cleanResults(listItems);
+  cleanResults(resultItems);
   serie = serieInput.value;
   getApiData();
 }
@@ -34,29 +36,15 @@ function printResults() {
     error.appendChild(errorContent);
     resultList.appendChild(error);
   } else {
-    for(let i = 0; i < results.length; i++) {
-      const newLi = document.createElement('li');
-      newLi.classList.add('serie-container');
-      newLi.setAttribute('id', results[i].show.id);
-      const liTitle = document.createElement('h2');
-      liTitle.classList.add('serie-title');
-      const liTitleContent = document.createTextNode(results[i].show.name);
-      liTitle.appendChild(liTitleContent);
-      newLi.appendChild(liTitle);
-      const liImg = document.createElement('IMG');
-      checkImg(i, liImg);
-      liImg.setAttribute('alt', results[i].show.name);
-      newLi.appendChild(liImg);
-      resultList.appendChild(newLi);
-    }
-    listItems = document.querySelectorAll('.serie-container');
-    createEventListener(listItems);
+    generateHTML(resultList, results);
+    resultItems = document.querySelectorAll('.serie-container');
+    createEventListener(resultItems);
   }
 }
 
 //clean the last result removing all child from the ul tag or the error message
 function cleanResults (items) {
-  //const listItems = document.querySelectorAll('.serie-container');
+  //const resultItems = document.querySelectorAll('.serie-container');
   for(const child of items) {
     resultList.removeChild(child);
   }
@@ -67,14 +55,15 @@ function cleanResults (items) {
 }
 
 //checkImg checks whether is an image on the results array//
-function checkImg(index, item) {
-  if(results[index].show.image === null) {
+function checkImg(list, index, item) {
+  if(list[index].show.image === null) {
     item.setAttribute('src', 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV');
   } else {
-    item.setAttribute('src', results[index].show.image.medium);
+    item.setAttribute('src', list[index].show.image.medium);
   }
 }
 
+//add the event listener for clicking series
 function createEventListener(lists) {
   for(const item of lists) {
     item.addEventListener('click', handlerClickfavourite);
@@ -83,8 +72,11 @@ function createEventListener(lists) {
 
 function handlerClickfavourite(ev) {
   let clickedItem = ev.currentTarget.id;
+  cleanFavourites(favouriteItems);
+  //cleanFavourites (favourites);
   //check if the new item is already at favourites
   const repeat = favourites.findIndex(favourite => favourite.show.id === parseInt(clickedItem));
+
   // //add to favourites the new series or remove the series already on favourites
   if(repeat === -1) {
     favourites.push(results.find(result => result.show.id === parseInt(clickedItem)));
@@ -92,6 +84,50 @@ function handlerClickfavourite(ev) {
   } else {
     highlightFavourites(clickedItem);
     favourites.splice(repeat,1);
+  }
+  for(let i = 0; i < favourites.length; i++) {
+    const newLi = document.createElement('li');
+    newLi.classList.add('serie-container-small');
+    newLi.setAttribute('id', `fav${favourites[i].show.id}`);
+    const liTitle = document.createElement('h2');
+    liTitle.classList.add('serie-title');
+    const liTitleContent = document.createTextNode(favourites[i].show.name);
+    liTitle.appendChild(liTitleContent);
+    newLi.appendChild(liTitle);
+    const liImg = document.createElement('IMG');
+    checkImg(favourites, i, liImg);
+    liImg.setAttribute('alt', favourites[i].show.name);
+    liImg.setAttribute('height', '100px');
+    newLi.appendChild(liImg);
+    favouriteList.appendChild(newLi);
+  }
+  favouriteItems = document.querySelectorAll('.serie-container-small');
+  resultItems = document.querySelectorAll('.serie-container');
+  //generateHTML(favouriteList, favourites);
+}
+
+function cleanFavourites (items) {
+  //const resultItems = document.querySelectorAll('.serie-container');
+  for(const child of items) {
+    favouriteList.removeChild(child);
+  }
+}
+
+function generateHTML(listcontainer, items) {
+  for(let i = 0; i < items.length; i++) {
+    const newLi = document.createElement('li');
+    newLi.classList.add('serie-container');
+    newLi.setAttribute('id', items[i].show.id);
+    const liTitle = document.createElement('h2');
+    liTitle.classList.add('serie-title');
+    const liTitleContent = document.createTextNode(items[i].show.name);
+    liTitle.appendChild(liTitleContent);
+    newLi.appendChild(liTitle);
+    const liImg = document.createElement('IMG');
+    checkImg(results, i, liImg);
+    liImg.setAttribute('alt', items[i].show.name);
+    newLi.appendChild(liImg);
+    listcontainer.appendChild(newLi);
   }
 }
 
